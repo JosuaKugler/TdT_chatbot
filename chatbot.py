c8cd6d4e711f592c25cc7ed2e6548c16b0d56930
niklas <niklas.dobberstein@gmx.de>
2019-09-22 14:07:07 +0200

    Changed new.txt to contain both datasets

--- chatbot.py
+++ chatbot.py
@@ -61,7 +61,9 @@ word_tokens = nltk.word_tokenize(raw)
 lemmer = WordNetLemmatizer()
 def LemTokens(tokens):
     return [lemmer.lemmatize(token) for token in tokens]
+
 remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
+
 def LemNormalize(text):
     return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
 
